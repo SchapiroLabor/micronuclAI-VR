c6655ce6493073c1061e94d34e4d4b245528ba50
Ibrahim <ibrahim9826@outlook.com>
2024-08-18 21:18:53 +0200

    Work on Home screen
    Appears to be that input fields require specific anchor and pivots

--- Assets/InputFields.cs
+++ Assets/InputFields.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
-using Vector3 = UnityEngine.Vector3;
-using Quaternion = UnityEngine.Quaternion;
-using Vectot2 = UnityEngine.Vector2;
+using UnityEngine.UI;
+using TMPro;
 
 
 
@@ -13,7 +11,7 @@ public class InputFields : MonoBehaviour
 
 
 
-    Transform Initialize(Transform MenuPanel)
+    public Transform Initialize(Transform MenuPanel)
     {   
         // Set MenuPanel as parent
         transform.SetParent(MenuPanel);
@@ -28,21 +26,11 @@ public class InputFields : MonoBehaviour
         // Set Pivot to centre
         GetComponent<RectTransform>().pivot = new UnityEngine.Vector2(0.5f, 0.5f);
 
-        // Add content size fitter
-        gameObject.AddComponent<UnityEngine.UI.ContentSizeFitter>().verticalFit = UnityEngine.UI.ContentSizeFitter.FitMode.PreferredSize;
-        gameObject.AddComponent<UnityEngine.UI.ContentSizeFitter>().horizontalFit = UnityEngine.UI.ContentSizeFitter.FitMode.PreferredSize;
-
-        // Organise children in vertical layout group
-        gameObject.AddComponent<UnityEngine.UI.VerticalLayoutGroup>();
-
         // Spacing set to 1 % of canvas height
-        gameObject.GetComponent<UnityEngine.UI.VerticalLayoutGroup>().spacing = GetComponent<RectTransform>().sizeDelta.y * 0.01f;
+        // gameObject.GetComponent<UnityEngine.UI.VerticalLayoutGroup>().spacing = GetComponent<RectTransform>().sizeDelta.y * 0.01f;
 
         // Position canvas at near clip plane
-        transform.localPosition = Vector3.zero;
-
-        // Face Canvas to player
-        transform.LookAt(Camera.main.transform);
+        //transform.localPosition = Vector3.zero;
 
 
         // Initialize input widget for image path
@@ -68,44 +56,117 @@ public class InputFields : MonoBehaviour
         // Set Pivot to centre
         inputWidget.GetComponent<RectTransform>().pivot = new UnityEngine.Vector2(0.5f, 0.5f);
 
-        // Set size to 50% of canvas for width and 10% for height
-        inputWidget.GetComponent<RectTransform>().sizeDelta = new UnityEngine.Vector2(Canvas.GetComponent<RectTransform>().sizeDelta.x * 0.5f, Canvas.GetComponent<RectTransform>().sizeDelta.y * 0.1f);
 
-        // Face Canvas to player
-        inputWidget.LookAt(Camera.main.transform);
 
         // Set scale to 1
         inputWidget.localScale = new Vector3(1, 1, 1);
 
-        // Set placeholder text
-        inputWidget.GetComponentInChildren<UnityEngine.UI.Text>().text = "Enter path to Input folder";
+        // Set the text of the placeholder
+        inputWidget.GetComponentInChildren<TextMeshProUGUI>().text = "Enter path to Input folder";
 
-        // Set input field text to empty
-        inputWidget.GetComponentInChildren<UnityEngine.UI.InputField>().text = "";
+        // Enable overflow
+        inputWidget.GetComponentInChildren<TextMeshProUGUI>().enableWordWrapping = true;
+
+        // Set color of text to grey
+        inputWidget.GetComponentInChildren<TextMeshProUGUI>().color = Color.black;
+
+
+
+                // Set all children of gametitle to size equal to parent
+        foreach (Transform child in inputWidget)
+        {
+            child.GetComponent<RectTransform>().sizeDelta = inputWidget.GetComponent<RectTransform>().sizeDelta;
+
+                        // anchor to bottom left
+            child.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
+            child.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0);
+
+            // Set pivot to bootom left
+            child.GetComponent<RectTransform>().pivot = new Vector2(0, 0);
+
+
+            // set all children of child to size equal to parent
+            foreach (Transform grandChild in child)
+            {
+                grandChild.GetComponent<RectTransform>().sizeDelta = child.GetComponent<RectTransform>().sizeDelta;
+
+                // anchor to bottom left
+                grandChild.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
+                grandChild.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0);
+
+                // Set pivot to bootom left
+                grandChild.GetComponent<RectTransform>().pivot = new Vector2(0, 0);
+
+                // Set local position to 0
+                grandChild.localPosition = new Vector3(0, 0, 0);
+            }
+        }
 
         return inputWidget;
     }
 
 
-    Transform InitializeInputWidget4PythonExec(Transform inputWidget=null)
+    Transform InitializeInputWidget4PythonExec(Transform Canvas)
     {   
-        if (inputWidget == null)
-        {
-            // Load the input widget
-             inputWidget = transform.GetChild(0);
-        }
-
-
-        // Make a copy of the input widget
-        inputWidget = Instantiate(inputWidget, transform);
+        // Load the input widget
+        Transform inputWidget = transform.GetChild(1);
 
         inputWidget.name = "PythonExecPath";
 
-        // Set placeholder text
-        inputWidget.GetComponentInChildren<UnityEngine.UI.Text>().text = "Path to Python Executable";
+                // Set anchors to bottom left
+        inputWidget.GetComponent<RectTransform>().anchorMin = new UnityEngine.Vector2(0, 0);
+        inputWidget.GetComponent<RectTransform>().anchorMax = new UnityEngine.Vector2(0, 0);
 
-        // Set input field text to empty
-        inputWidget.GetComponentInChildren<UnityEngine.UI.InputField>().text = "";
+        // Set Pivot to centre
+        inputWidget.GetComponent<RectTransform>().pivot = new UnityEngine.Vector2(0.5f, 0.5f);
+
+
+                // Set scale to 1
+        inputWidget.localScale = new Vector3(1, 1, 1);
+
+
+        // Set placeholder text
+        inputWidget.GetComponentInChildren<TextMeshProUGUI>().text  = "Path to Python Executable";
+
+        // Enable overflow
+        inputWidget.GetComponentInChildren<TextMeshProUGUI>().enableWordWrapping = true;
+
+        // Set color of text to grey
+        inputWidget.GetComponentInChildren<TextMeshProUGUI>().color = Color.black;
+
+
+                        // Set all children of gametitle to size equal to parent
+        foreach (Transform child in inputWidget)
+        {
+            child.GetComponent<RectTransform>().sizeDelta = inputWidget.GetComponent<RectTransform>().sizeDelta;
+
+                        // anchor to bottom left
+            child.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
+            child.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0);
+
+            // Set pivot to bootom left
+            child.GetComponent<RectTransform>().pivot = new Vector2(0, 0);
+
+            // Set local position to 0
+            child.localPosition = new Vector3(0, 0, 0);
+
+            // set all children of child to size equal to parent
+            foreach (Transform grandChild in child)
+            {
+                grandChild.GetComponent<RectTransform>().sizeDelta = child.GetComponent<RectTransform>().sizeDelta;
+
+                // anchor to bottom left
+                grandChild.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
+                grandChild.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0);
+
+                // Set pivot to bootom left
+                grandChild.GetComponent<RectTransform>().pivot = new Vector2(0, 0);
+
+                // Set local position to 0
+                grandChild.localPosition = new Vector3(0, 0, 0);
+            }
+        }
+
 
         return inputWidget;
     }
--- Assets/Scenes/CIAnnotator/LoadScene.cs
+++ Assets/Scenes/CIAnnotator/LoadScene.cs
@@ -1,16 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class LoadScene : MonoBehaviour
 {
     // Start is called before the first frame update
+
     void Start()
-    {
-        Transform canvas = InitializeCanvas();   
+    {   
+        Transform canvas = InitializeMenuPanel();   
     }
 
-    Transform InitializeCanvas()
+    Transform InitializeMenuPanel()
     {
         // Load the canvas
         gameObject.name = "MenuPanel";
@@ -22,16 +30,157 @@ public class LoadScene : MonoBehaviour
         // Set Pivot to centre
         GetComponent<RectTransform>().pivot = new Vector2(0.5f, 0.5f);
 
-        // Set Canvas same size as fov
-        GetComponent<RectTransform>().sizeDelta = new Vector2(Camera.main.pixelWidth, Camera.main.pixelHeight);
+        // Compute FOV from anglar field of view and distance to near clip plane
+        /*float fov_vertical = (float) (2 * Math.Tan(camera.fieldOfView * 0.5f) * camera.nearClipPlane);
 
-        // Position canvas at near clip plane
-        transform.position = Camera.main.transform.position + Camera.main.transform.forward * Camera.main.nearClipPlane;
+        // Fov horizontal
+        float fov_horizontal = (float) (fov_vertical * camera.aspect); */
 
-        // Face Canvas to player
-        transform.LookAt(Camera.main.transform);
+
+        // Set at camera poistion and rotation and at far clip plane. Near clip plane does not work in 2D
+        // Get render camera from canvas
+        Camera camera = GetComponent<Canvas>().worldCamera;
+        // Set plane distance at far clip plane
+        GetComponent<Canvas>().planeDistance = camera.farClipPlane;
+
+        // Initialize input widget for image path
+        Transform inputWidget = transform.GetComponentInChildren<InputFields>().Initialize(transform);
+
+        // Initialize next button
+        Transform nextButton = InitializeNextButton();
+
+        // Initialize lab logo
+        InitializeLabLogo(transform);
+
+        // Initialize game title
+        InitializeGameTitle(transform);
 
         return transform;
+    }
+
+
+    Transform InitializeNextButton()
+
+    {  
+        // Get NextButton from MenuPanel
+        Transform nextButton = transform.GetChild(0);
+
+        /*
+        nextButton.name = "NextButton";
+
+        // Set anchors to bottom left
+        nextButton.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
+        nextButton.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0);
+
+        // Set Pivot to centre
+        nextButton.GetComponent<RectTransform>().pivot = new Vector2(0.5f, 0.5f);
+
+        // Set scale equal to parent
+        nextButton.localScale = new Vector3(1, 1, 1);
+
+        // Set button size to 20 % of canvas width and 20 % of canvas height
+        //nextButton.GetComponent<RectTransform>().sizeDelta = new Vector2(GetComponent<RectTransform>().sizeDelta.x * 0.2f, GetComponent<RectTransform>().sizeDelta.y * 0.2f);
+
+        // Set button position to 75% + (5%/2) of canvas width and 50% + (5%/2) of canvas height
+        nextButton.position = new Vector3(GetComponent<RectTransform>().sizeDelta.x * 0.75f, 
+        GetComponent<RectTransform>().sizeDelta.y * 0.5f + GetComponent<RectTransform>().sizeDelta.y * 0.05f * 0.5f, 0);
+
+        // Set button text size to 1% of button size
+        nextButton.GetComponentInChildren<TextMeshProUGUI>().fontSize = Mathf.FloorToInt(nextButton.GetComponent<RectTransform>().sizeDelta.x * 0.01f);
+
+        // Set button text colour
+        nextButton.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
+
+        // Set button colour
+        nextButton.GetComponent<Image>().color = Color.black;
+
+
+        // Set button size to fit the text
+        nextButton.GetComponent<RectTransform>().sizeDelta = new Vector2(nextButton.GetComponentInChildren<TextMeshProUGUI>().preferredWidth, nextButton.GetComponentInChildren<TextMeshProUGUI>().preferredHeight);
+        */
+
+        // Add button click event
+        nextButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() => { OnConfirmButtonClick(); });
+
+
+
+        return nextButton;
+    }
+
+    void OnConfirmButtonClick()
+    {
+        // Load the next scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("CIAnnotator");
+
+    }
+
+
+    void InitializeLabLogo(Transform Canvas)
+    {
+        // Load the lab logo
+        Transform labLogo = Canvas.GetChild(1);
+        labLogo.name = "LabLogo";
+
+        /*
+        // Set anchors to top left
+        labLogo.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
+        labLogo.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0);
+
+        // Set Pivot to centre
+        labLogo.GetComponent<RectTransform>().pivot = new Vector2(0.5f, 0f);
+
+        // Set size to 50% of canvas for width and 30% for height
+        labLogo.GetComponent<RectTransform>().sizeDelta = new Vector2(Canvas.GetComponent<RectTransform>().sizeDelta.x * 0.75f, Canvas.GetComponent<RectTransform>().sizeDelta.y * 0.3f);
+
+        // Set position to bottom centre of canvas
+        labLogo.localPosition = new Vector3(Canvas.GetComponent<RectTransform>().sizeDelta.x * 0.5f, 0, 0);
+
+        // Set scale to 1
+        labLogo.localScale = new Vector3(1, 1, 1); */
+
+        // Set image
+        labLogo.GetComponent<RawImage>().texture = Resources.Load<Texture>("Assets/Scenes/CIAnnotator/LabImage.jpg");
+
+
+        
+
+    }
+
+
+    void InitializeGameTitle(Transform Canvas)
+    {
+
+
+        // Load a prefab text object from Assets
+        Transform gameTitle = Canvas.GetChild(3);
+
+/*
+        // Set anchors to top centre
+        gameTitle.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
+        gameTitle.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0);
+
+        // Set Pivot to centre
+        gameTitle.GetComponent<RectTransform>().pivot = new Vector2(0.5f, 1.0f);
+
+           // Set button size to fit the text
+        gameTitle.GetComponent<RectTransform>().sizeDelta = new Vector2(gameTitle.GetComponent<TextMeshProUGUI>().preferredWidth,
+        gameTitle.GetComponent<TextMeshProUGUI>().preferredHeight);
+
+
+        // Make text bold
+        gameTitle.GetComponent<TextMeshProUGUI>().fontStyle = TMPro.FontStyles.Bold;
+
+        // Set scale to 1
+        gameTitle.localScale = new Vector3(1, 1, 1); */
+
+        // Set text
+        gameTitle.GetComponent<TextMeshProUGUI>().text = "MicroNuclAI virtual reality annotation tool";
+
+        // Set text colour
+        gameTitle.GetComponent<TextMeshProUGUI>().color = Color.black;
+
+
+
     }
 
 
@@ -39,3 +188,4 @@ public class LoadScene : MonoBehaviour
 
 
 }
+
