438e0be517b925c58d57dfc49d3ce10ac3b63aa3
Ibrahim <92687705+Ibrahim-norbert@users.noreply.github.com>
2025-04-20 15:35:19 +0200

    Refactoring
    Cleaning

--- napari_pluginTest.py
+++
@@ -1,5 +0,0 @@
-# Load image
-img = np.moveaxis(tf.imread(r"C:\Schapiro Lab\ibrahim_hiwi\VRproject\VR Demo dataset\image_34.tif"), 0, -1)
-viewer = npi.Viewer(title="Test")
-viewer.add_image(img, channel_axis=-1)
-viewer.show()
